e0b8a335a559d283b22d7c07b5dcc29e7627153a
Paulo Roberto Antunes De Carvalho <paulorantunes13@hotmail.com>
2024-07-13 23:35:00 -0300

    feat: Criação do componente Carousel para a listagem dos títulos

---
+++ animesFound/src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,13 @@
+import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.scss',
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+})
+export class CarouselComponent {
+
+}
