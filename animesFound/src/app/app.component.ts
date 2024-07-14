710d2d6524c7564402257d6341f8b7dcd2f80942
Paulo Roberto Antunes De Carvalho <paulorantunes13@hotmail.com>
2024-07-14 00:29:27 -0300

    chore: Adicionando o componente Home para o componente principal

--- animesFound/src/app/app.component.ts
+++ animesFound/src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { SlideMenuComponent } from './components/slide-menu/slide-menu.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    SlideMenuComponent,
-    SideMenuComponent
+    HomeComponent,
+    FooterComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
