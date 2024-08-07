a596f154dcf28295e5a875356c4c346d74f44c24
Paulo Roberto Antunes De Carvalho <paulorantunes13@hotmail.com>
2024-08-07 00:29:22 -0300

    fix: Ajustando as classes de estilização e conteúdo html do componente Home

---
+++ animesFound/src/app/pages/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component , CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { SlideMenuComponent } from '../../components/slide-menu/slide-menu.component';
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
+import { CarouselComponent } from '../../components/carousel/carousel.component';
+import { AccordionComponent } from '../../components/accordion/accordion.component';
+import { NewsComponent } from '../../components/news/news.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    SlideMenuComponent,
+    SideMenuComponent,
+    CarouselComponent,
+    AccordionComponent,
+    NewsComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+})
+export class HomeComponent {
+
+}
