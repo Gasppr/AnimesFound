3ee3dde40c7967c3c7000ecb681fefc723e201fd
Paulo Roberto Antunes De Carvalho <paulorantunes13@hotmail.com>
2024-08-07 00:30:24 -0300

    feat: Atualizando informações do componente footer e estrutura estética do componente.

--- animesFound/src/app/app.component.ts
+++ animesFound/src/app/app.component.ts
@@ -3,9 +3,9 @@ import { RouterOutlet } from '@angular/router';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { SlideMenuComponent } from './components/slide-menu/slide-menu.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { IntroComponent } from './components/intro/intro.component';
+import { IntroComponent } from './pages/intro/intro.component';
 
 @Component({
   selector: 'app-root',
--- animesFound/src/app/app.routes.ts
+++ animesFound/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { IntroComponent } from './pages/intro/intro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '' , component: HomeComponent},
+  {path: "home" ,component: HomeComponent},
+  {path: "intro", component:IntroComponent}
+];
